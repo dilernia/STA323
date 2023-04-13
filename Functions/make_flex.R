e20f698b319be0626ad0bd8cbbb1a5ab3ecedde9
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2023-04-13 16:05:16 -0400

    Create make_flex.R

---
+++ Functions/make_flex.R
@@ -0,0 +1,28 @@
+# Function for making flextable
+make_flex <- function(myData, ndigits = 2, caption = NULL) {
+  
+  if("p.value" %in% colnames(myData)) {
+    myData$p.value <- format.pval(myData$p.value, digits = ndigits)
+  }
+  
+  myFlex <- myData %>% 
+    flextable::flextable() %>% 
+    flextable::autofit() %>% 
+    flextable::colformat_double(digits = ndigits) %>% 
+    flextable::fit_to_width(7.5)
+  
+    if(!exists("tbl_counter")) {
+  assign("tbl_counter", 
+         value = 1, envir = .GlobalEnv)
+  }
+  
+  if(!is.null(caption)) {
+        myFlex <- myFlex %>% 
+      flextable::set_caption(paste0("Table ", tbl_counter, ": ", caption))
+  }
+  
+  assign("tbl_counter", 
+         value = tbl_counter + 1, envir = .GlobalEnv)
+  
+   return(myFlex) 
+}
