d1c7f64bcf4b84ee3a259f4fec24063893b46fc0
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2023-04-13 15:57:56 -0400

    Create vif_plot.R

---
+++ Functions/vif_plot.R
@@ -0,0 +1,26 @@
+# Function for plotting variance inflation factor (VIF) values 
+vif_plot <- function(modFit) {
+  
+  vifs <- car::vif(modFit)
+  
+  if("GVIF^(1/(2*Df))" %in% colnames(vifs)) {
+    vifs <- vifs[, "GVIF^(1/(2*Df))"]
+  }
+  
+  preds <- names(vifs)
+  
+  vifGG <- tibble(Predictor = preds,
+       VIF = vifs) %>% 
+    dplyr::mutate(Predictor = fct_reorder(Predictor, -VIF)) %>% 
+    ggplot(aes(x = VIF, y = Predictor)) + 
+    geom_segment(aes(xend = VIF, x = 0, 
+                     yend = Predictor, y = Predictor)) +
+    geom_vline(xintercept = 5, linetype = "dotted") +
+    geom_vline(xintercept = 10, linetype = "dotted") +
+    scale_x_continuous(limits = c(0, max(vifs)*1.1),
+                       expand = expansion(mult = c(0, 0.10))) +
+    geom_point(size = 3, color = "steelblue") +
+    labs(title = "Variance inflation values")
+  
+return(suppressWarnings(print(vifGG)))
+}
